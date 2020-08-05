de95ef640f73697c983bbceee74662ef3f0fc70e
stefanodotit <4629716+stefanodotit@users.noreply.github.com>
2020-08-05 19:05:23 +0200

    feat: fix questions

--- store/questions.js
+++ store/questions.js
@@ -1,3 +1,6 @@
+import { IS_PROD } from '~~/utils'
+import * as questions from '~~/data/questions.json'
+
 export const state = () => ({
   questions: []
 })
@@ -8,12 +11,17 @@ export const getters = {
 
 export const actions = {
   add(store, obj) {
+    if (IS_PROD) return
     return this.$axios.post('/api/add', obj)
   },
   load({ commit }) {
+    if (IS_PROD) {
+      return commit('load', questions.default.questions)
+    }
     return this.$axios.get('/api/load').then(({ data }) => commit('load', data))
   },
   edit(store, obj) {
+    if (IS_PROD) return
     return this.$axios.post('/api/edit', obj)
   }
 }

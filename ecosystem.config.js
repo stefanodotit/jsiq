446c46655924a6dee7e45402c5fc2005a9c0bcf6
stefanodotit <4629716+stefanodotit@users.noreply.github.com>
2020-08-05 08:57:22 +0200

    ci: fix pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start'
+      args: 'start --port 80'
     }
   ]
 }
